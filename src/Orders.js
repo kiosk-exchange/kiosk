bfb3696997dff5ec4b80e5408c6dcb2fa226a3e2
richmcateer <rmcateer1@gmail.com>
2017-07-25 23:14:42 -0400

    Minor changes

--- src/Orders.js
+++ src/Orders.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react'
 import { Table } from 'react-bootstrap'
 
+import getWeb3 from './utils/getWeb3'
+
+import publicProductABI from '../build/contracts/PublicProduct.json'
+const contract = require('truffle-contract')
+
 import './Orders.css'
 
 class Orders extends Component {
+
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			web3: null,
+			publicProduct: null
+		}
+
+	}
+
+	 componentWillMount() {
+    getWeb3.then(results => {
+      this.setState({
+        web3: results.web3,
+      })
+
+      this.initializePublicProduct()
+    })
+  }
+
+  initializePublicProduct() {
+  	const publicProduct = contract(publicProductABI)
+    publicProduct.setProvider(this.state.web3.currentProvider)
+    publicProduct.deployed().then((instance) => {
+    	console.log(instance)
+      this.setState({ publicProduct: instance.contract })
+    })
+  }
+
 
 	render() {
 		return (
