030e7852406cbdb560043e01531eba6de2e0e801
richmcateer <rmcateer1@gmail.com>
2017-07-26 19:23:40 -0400

    Spacing

--- src/Products.js
+++ src/Products.js
@@ -5,7 +5,7 @@ import getWeb3 from './utils/getWeb3'
 
 class Products extends Component {
 
-	constructor(props) {
+  constructor(props) {
     super(props)
 
     this.state = {
@@ -30,39 +30,39 @@ class Products extends Component {
   }
 
   render() {
-		return (
-			<div>
+    return (
+      <div>
 
         <div className="container-products-table">
 
           <div className="container-products-header">
-    				<h1 className="products-header">Products</h1>
+            <h1 className="products-header">Products</h1>
             <button className="add-product-button" onClick={this.handleAddProduct}>
-              Add Product
+            Add Product
             </button>
           </div>
 
           <div className="products-table">
-    				<Table striped bordered condensed hover>
-    					<tr>
-    						<th>DIN</th>
-    						<th>Name</th>
-    						<th>Image</th>
-    					</tr>
+            <Table striped bordered condensed hover>
+            <tr>
+              <th>DIN</th>
+              <th>Name</th>
+              <th>Image</th>
+            </tr>
 
-    					<tbody>
-    						<tr>
-    							<td>1000-0001</td>
-    							<td>Blue T-Shirt</td>
-    							<td>blah</td>
-    						</tr>
-    					</tbody>
-    				</Table>
+              <tbody>
+                <tr>
+                	<td>1000-0001</td>
+                	<td>Blue T-Shirt</td>
+                	<td>blah</td>
+                </tr>
+              </tbody>
+            </Table>
           </div>
 
         </div>
 
-			</div>
+      </div>
       
 		);
   }
