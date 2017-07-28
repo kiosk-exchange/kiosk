2a465e982e751c61d3ee53d383bc124fbaa70ef9
richmcateer <rmcateer1@gmail.com>
2017-07-27 23:00:38 -0400

    Market

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.4.11;
 *  This is the interface for a market (beta).
 */
 contract Market {
-	// function inStock(uint256 productID, uint256 quantity) constant returns (bool);
-	// function price(uint256 productID) constant returns (uint256);
-	// function buy(uint256 productID, uint256 quantity) payable;
+	function inStock(uint256 productID, uint256 quantity) constant returns (bool);
+	function price(uint256 productID) constant returns (uint256);
+	function buy(uint256 productID, uint256 quantity) payable;
 }
