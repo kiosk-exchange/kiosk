278659660f899c3cf1adbeff1140298a332dc34c
richmcateer <rmcateer1@gmail.com>
2017-08-01 15:21:10 -0400

    Compiles

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.4.11;
 *  This is the interface for a market (beta).
 */
 contract Market {
-	function inStock(uint256 productID, uint256 quantity) constant returns (bool);
-	function price(uint256 productID) constant returns (uint256);
-	function buy(uint256 productID, uint256 quantity) payable;
+	// function isAvailableForSale(uint256 DIN, uint256 quantity) constant returns (bool);
+	// function unitPrice(uint256 DIN) constant returns (uint256);
+	// function totalPrice(uint256 DIN, uint256 quantity) constant returns (uint256);
+	function inStock(uint256 DIN, uint256 quantity) constant returns (bool);
+	function price(uint256 DIN) constant returns (uint256);
+	function buy(uint256 DIN, uint256 quantity) payable;
 }
+
