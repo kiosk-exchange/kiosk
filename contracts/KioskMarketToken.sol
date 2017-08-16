e4a07a69b79605d9ceceb7d61a79c47b0563eb6f
richmcateer <rmcateer1@gmail.com>
2017-08-16 16:45:13 -0400

    Spacing

--- contracts/KioskMarketToken.sol
+++ contracts/KioskMarketToken.sol
@@ -8,9 +8,9 @@ import "zeppelin-solidity/contracts/token/StandardToken.sol";
 // https://github.com/ConsenSys/Tokens/blob/master/contracts/HumanStandardToken.sol
 
 contract KioskMarketToken is StandardToken {
-  string public name = "Kiosk Market Token";							// Set the name for display purposes
-  string public symbol = "KMT";														// Set the symbol for display purposes
-  uint256 public decimals = 18;														// Amount of decimals for display purposes
+  string public name = "Kiosk Market Token";      // Set the name for display purposes
+  string public symbol = "KMT";                   // Set the symbol for display purposes
+  uint256 public decimals = 18;                   // Amount of decimals for display purposes
 
   // The address of DIN registry where all DINs are stored.
   DINRegistry public dinRegistry;
@@ -24,7 +24,7 @@ contract KioskMarketToken is StandardToken {
     uint256 _totalSupply
   ) 
   {
-    balances[msg.sender] = _totalSupply * 10**decimals;		// Give the creator all initial tokens
+    balances[msg.sender] = _totalSupply * 10**decimals;   // Give the creator all initial tokens
     totalSupply = _totalSupply * 10**decimals;            // Update total supply
   }
 
