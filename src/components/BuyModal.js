7e5430e799615850ffd0616a8eee4aa8ad821f3f
richmcateer <rmcateer1@gmail.com>
2017-08-26 13:41:09 -0400

    More buy modal styling

--- src/components/BuyModal.js
+++ src/components/BuyModal.js
@@ -1,61 +1,115 @@
-import React from 'react';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Dialog from "material-ui/Dialog";
+import RaisedButton from "material-ui/RaisedButton";
 import QuantityPicker from "./QuantityPicker";
+import Subheader from "material-ui/Subheader";
 
 const styles = {
   radioButton: {
-    marginTop: 16,
-  },
+    marginTop: 16
+  }
 };
 
 /**
  * Dialog content can be scrollable.
  */
-export default class DialogExampleScrollable extends React.Component {
+class BuyModal extends Component {
   state = {
-    open: false,
+    open: false
   };
 
   handleOpen = () => {
-    this.setState({open: true});
+    this.setState({ open: true });
   };
 
   handleClose = () => {
-    this.props.handleClose()
-    this.setState({open: false});
+    this.props.handleClose();
+    this.setState({ open: false });
   };
 
   render() {
+    const contentStyle = {
+      width: "50%",
+      minWidth: "300px",
+      maxWidth: "500px"
+    };
+
+    const subheaderStyle = {
+      color: "#9CA6AF",
+      letterSpacing: "1px",
+      fontSize: "16px",
+      fontWeight: "medium",
+      padding: "0px"
+    };
+
     const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
+      <RaisedButton
+        label="Buy Now"
+        backgroundColor={this.context.theme.blue}
+        labelColor="#FFFFFF"
+        fullWidth={true}
         onClick={this.handleClose}
-      />,
-      <FlatButton
-        label="Buy"
-        primary={true}
-        keyboardFocused={false}
-        onClick={this.handleClose}
-      />,
+      />
     ];
 
     return (
       <div>
         <Dialog
-          title={this.props.product.name}
+          title={
+            <div
+              style={{ textAlign: "center", color: this.context.theme.gray }}
+            >
+              {this.props.product.name}
+              <Subheader style={subheaderStyle}>
+                {this.props.product.DIN}
+              </Subheader>
+            </div>
+          }
           actions={actions}
+          actionsContainerStyle={{ padding: "20px 10%" }}
           modal={false}
+          contentStyle={contentStyle}
           open={this.props.open}
           onRequestClose={this.props.handleClose}
           autoScrollBodyContent={true}
         >
-        <p>{this.props.product.DIN}</p>
-        <QuantityPicker />
-        <p>{this.props.product.price}</p>
+          <div style={{ color: this.context.theme.gray }}>
+            <div style={{ display: "flex" }}>
+              <h2>Quantity</h2>
+              <div
+                style={{
+                  width: "100%",
+                  padding: "0px 20px",
+                  textAlign: "right"
+                }}
+              >
+                <h2>1</h2>
+              </div>
+            </div>
+            <div style={{ display: "flex" }}>
+              <h2>Total</h2>
+              <div
+                style={{
+                  width: "100%",
+                  padding: "0px 20px",
+                  textAlign: "right"
+                }}
+              >
+                <h2>
+                  {this.props.product.price + " KMT"}
+                </h2>
+              </div>
+            </div>
+          </div>
         </Dialog>
       </div>
-    )
+    );
   }
 }
+
+BuyModal.contextTypes = {
+  theme: PropTypes.object
+};
+
+export default BuyModal;
