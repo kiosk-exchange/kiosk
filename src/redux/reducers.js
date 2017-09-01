dffa0e711cf026c93eb87d5e7203a10e1f2e9e52
richmcateer <rmcateer1@gmail.com>
2017-09-01 19:12:47 -0400

    Simplified redux working

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
 import {
 	WEB_3_LOADING,
@@ -32,29 +32,26 @@ const reducer = (state, action, type) => {
 *  Name reducers according to how you would like to access  them in a component (e.g. web3 instead of web3Success)
 *  Use a separate reducer for each top-level object in the state tree.
 */
-export const web3IsLoading = (state = false, action) => 
-	reducer(state, action, WEB_3_LOADING)
-
-export const web3HasError = (state, action) => reducer(state = false, action, WEB_3_ERROR);
-export const web3 = (state, action) => reducer(state = null, action, WEB_3_SUCCESS);
-export const accountHasError = (state, action) => reducer(state = false, action, ACCOUNT_ERROR);
-export const account = (state, action) => reducer(state = null, action, ACCOUNT_SUCCESS);
-export const networkHasError = (state, action) => reducer(state = false, action, NETWORK_ERROR);
-export const network = (state, action) => reducer(state = null, action, NETWORK_SUCCESS);
-export const KMTContract = (state, action) => reducer(state = null, action, KMT_CONTRACT);
-export const KMTBalance = (state, action) => reducer(state = null, action, KMT_BALANCE);
-export const ETHBalance = (state, action) => reducer(state = null, action, ETH_BALANCE);
+const web3IsLoading = (state = false, action) => reducer(state, action, WEB_3_LOADING);
+const web3HasError = (state = false, action) => reducer(state, action, WEB_3_ERROR);
+const web3 = (state = null, action) => reducer(state, action, WEB_3_SUCCESS);
+const accountHasError = (state = false, action) => reducer(state, action, ACCOUNT_ERROR);
+const account = (state = null, action) => reducer(state, action, ACCOUNT_SUCCESS);
+const networkHasError = (state = false, action) => reducer(state, action, NETWORK_ERROR);
+const network = (state = null, action) => reducer(state, action, NETWORK_SUCCESS);
+const KMTContract = (state = null, action) => reducer(state, action, KMT_CONTRACT);
+const KMTBalance = (state = null, action) => reducer(state, action, KMT_BALANCE);
+const ETHBalance = (state = null, action) => reducer(state, action, ETH_BALANCE);
 
 export const rootReducer = combineReducers({
 	web3IsLoading,
-	// web3HasError,
-	// web3,
-	// accountHasError,
-	// account,
-	// networkHasError,
-	// network,
-	// KMTContract,
-	// KMTBalance,
-	// ETHBalance
+	web3HasError,
+	web3,
+	accountHasError,
+	account,
+	networkHasError,
+	network,
+	KMTContract,
+	KMTBalance,
+	ETHBalance
 });
-
