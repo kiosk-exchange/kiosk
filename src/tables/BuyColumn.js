403c88806f68b9b0b2ae85fa0f6b75469bc203b9
richmcateer <rmcateer1@gmail.com>
2017-09-02 04:33:41 -0400

    Start on modal logic

--- src/tables/BuyColumn.js
+++ src/tables/BuyColumn.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import RaisedButton from "material-ui/RaisedButton";
 import { TableRowColumn } from "material-ui/Table";
+import { connect } from "react-redux";
+import { showBuyModal } from "../redux/actions";
+
+mapDispatchToProps = (dispatch, ownProps) => ({
+	onClick: (ownProps.product) => {
+		dispatch(showBuyModal(product))
+	}
+})
 
 class BuyColumn extends Component {
 	render() {
+		const { product, onClick } = this.props
+
 		return (
 			<TableRowColumn>
 				<RaisedButton
 					label="Buy"
-					disabled={!this.props.product.available}
+					disabled={false}
 					backgroundColor="#32C1FF"
 					labelColor="#FFFFFF"
-					onClick={() => this.props.handleBuyClick(this.props.product)}
+					// onClick={() => this.props.handleBuyClick(this.props.product)}
 				/>
 			</TableRowColumn>
 		);
 	}
 }
 
-export default BuyColumn;
+export default connect(null, mapDispatchToProps)(BuyColumn);
