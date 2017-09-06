24cbc3c8e58280b44d8dfce76e276d2cd24c789a
richmcateer <rmcateer1@gmail.com>
2017-09-06 11:28:33 -0400

    Image works

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -54,13 +54,14 @@ const SideMenu = ({ account, KMT, ETH, selectedItem, selectMenuItem }) => {
 		style: style
 	};
 
+
 	return (
 		<SelectableList style={listStyle} value={selectedItem}>
 			<ListItem
 				style={logoStyle}
 				disabled={true}
 				primaryText="kiosk"
-				leftAvatar={<Avatar src="favicon.png" backgroundColor="none" />}
+				leftAvatar={<Avatar src={"./favicon.png"} backgroundColor="none" />}
 			/>
 			<br />
 			<Subheader style={subheaderStyle}>BUY</Subheader>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { routerMiddleware } from 'react-router-redux'
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from "./redux/reducers";
 import { Router, Route } from "react-router";
-import { configureStore, history } from "./redux/configureStore"
+import createHistory from "history/createBrowserHistory";
 
 import App from "./App";
 import "./styles/App.css";
@@ -20,7 +25,16 @@ const initialState = {
 	}
 };
 
-const store = configureStore(initialState)
+const history = createHistory();
+
+
+const routing = routerMiddleware(history);
+
+const store = createStore(
+	rootReducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(thunk, routing))
+);
 
 render(
 	<Provider store={store}>
@@ -32,12 +46,13 @@ render(
 					path="/hello"
 					render={() => <h1>Hello, World!</h1>}
 				/>
+				<Route path="/market/:market" render={() => <App />} />
 			</div>
 		</Router>
 	</Provider>,
 	document.getElementById("root")
 );
 
-// if (module.hot) {
-// 	module.hot.accept();
-// }
+if (module.hot) {
+	module.hot.accept();
+}
