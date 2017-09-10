2d5ec1f61b4e5990da50e5084f4f153e847cee03
richmcateer <rmcateer1@gmail.com>
2017-09-10 16:28:57 -0400

    More

--- contracts/Kiosk.sol
+++ contracts/Kiosk.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.4.11;
 
-import "./KioskMarketToken.sol";
-
 contract Kiosk {
     /**
     *  ==============================
@@ -9,17 +7,16 @@ contract Kiosk {
 	*  ==============================
     */
 
-    // The address of the Kiosk Market Token contract.
-    address public KMT;
-
-    // Owner will be a DAO that manages Kiosk protocol contract upgrades based on KMT ownership.
+    // In production, owner will be a DAO that approves contract upgrades based on KMT ownership.
+    // While in beta, owner is the Kiosk developers.
     address public owner;
 
-    // The address of the Buy contract.
-    address public buy;
+    // Buy contract address => Valid 
+    // This allows us to add functionality while still allowing older versions of Buy to function.
+    mapping (address => bool) public validBuyAddresses;
 
     // The address of the DINRegistry contract.
-    address public registry;							
+    address public registry;
 
     // The address of DINRegistrar contract.
     address public registrar;
@@ -36,10 +33,14 @@ contract Kiosk {
     }
 
     // Constructor
-	function Kiosk(KioskMarketToken _KMT) {
-		KMT = _KMT;
+	function Kiosk() {
 		owner = msg.sender;
 	}
+
+    // Kiosk Market Token will use this do determine whether to accept a transferFromBuy call
+    function isValid(address _buy) constant returns (bool) {
+    	return validBuys[_buy];
+    }
 
     /**
     *  ==============================
@@ -47,16 +48,12 @@ contract Kiosk {
     *  ==============================
     */
 
-    function setKMT(address _KMT) only_owner {
-    	KMT = _KMT;
-    }
-
     function setOwner(address _owner) only_owner {
         owner = _owner;
     }
 
-    function setBuy(address _buy) only_owner {
-        buy = _buy;
+    function setBuy(address _buy, bool _valid) only_owner {
+    	validBuyAddresses[_buy] = _valid;
     }
 
     function setRegistry(address _registry) only_owner {
--- contracts/KioskMarketToken.sol
+++ contracts/KioskMarketToken.sol
@@ -1,43 +1,34 @@
 pragma solidity ^0.4.11;
 
+import "./Kiosk";
 import "zeppelin-solidity/contracts/token/StandardToken.sol";
 
 contract KioskMarketToken is StandardToken {
     string public name = "Kiosk Market Token";      // Set the name for display purposes
     string public symbol = "KMT";                   // Set the symbol for display purposes
     uint256 public decimals = 18;                   // Amount of decimals for display purposes
+    Kiosk public kiosk;                             // The address of the Kiosk protocol contract
 	
     // Constructor
-    function KioskMarketToken(uint256 _totalSupply) {
+    function KioskMarketToken(Kiosk _kiosk, uint256 _totalSupply) {
+    	kiosk = _kiosk;
         balances[msg.sender] = _totalSupply;        // Give the creator all initial tokens
         totalSupply = _totalSupply;                 // Update total supply
     }
 
     /**
     *  ==============================
-    *         ERC20 Overrides
+    *          ERC20 Extension
     *  ==============================
     */
-	
-    function transfer(address _to, uint256 _value) returns (bool) {
-        // Do not allow transfers to this contract or the null address.
-        require(_to != address(this) && _to != address(0));
-        return super.transfer(_to, _value);
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
-        // Do not allow transfers to this contract or the null address.
-        require(_to != address(this) && _to != address(0));
+    function transferFromBuy(address _from, address _to, uint256 _value) returns (bool) {
+        require (kiosk.isValid(msg.sender) == true);
 
         // Allow Buy contract to have full discretion over a user's balance.
-        if (msg.sender == buy) {
-            balances[_to] = balances[_to].add(_value);
-            balances[_from] = balances[_from].sub(_value);
-            Transfer(_from, _to, _value);
-            return true;
-        }
-
-        return super.transferFrom(_from, _to, _value);
+        balances[_to] = balances[_to].add(_value);
+        balances[_from] = balances[_from].sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
     }
 
 }
